93632033f3f8e7d538aa708dfb24090db51f97c9
Monisha <monisha55601@gmail.com>
2026-06-30 16:01:04 +0000

    Code challenge submission for question: 1281 Subtract the Product and Sum of Digits of an Integer

---
+++ 1281 Subtract the Product and Sum of Digits of an Integer/1281subtract-the-product-and-sum-of-digits-of-an-integer.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int r,p=1,s=0;
+        while(n>0){
+            r=n%10;
+            p*=r;
+            s+=r;
+            n/=10;
+        }
+        return p-s;
+    }
+}
